7235afd14438843f798b6ff897993b233f42963b
Benny Chrispin <ndizibaidu23@gmail.com>
2023-11-13 17:07:10 +0200

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,68 @@
-import logo from './logo.svg';
-import './App.css';
+import "./index.css";
+import { useState } from "react";
 
-function App() {
+const questions = [
+  {
+    id: 1,
+    question: "What language is React based on?",
+    answer: "React is built using JavaScript.",
+  },
+  {
+    id: 2,
+    question: "Why do we need to install NodeJS for frontend development?",
+    answer: "To run the npm commands",
+  },
+  {
+    id: 3,
+    question:
+      "How does a developer become proficient in programming languages like Java and Python?",
+    answer: "Through practice, patience, and dedication.",
+  },
+  {
+    id: 4,
+    question: "Which of these are not used as keywords in HTML?",
+    answer: "<div>, <span>,<p>",
+  },
+  {
+    id: 5,
+    question: "Which one of this statement about Javascript is true? ",
+    answer: "Javascript is a server-side scripting language.",
+  },
+  {
+    id: 6,
+    question: "what is react?",
+    answer: "react is javascript library",
+  },
+];
+
+export default function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <FlashCard />
     </div>
   );
 }
 
-export default App;
+function FlashCard() {
+  const [selectedId, setSelectedId] = useState(5);
+
+  function handleClick(id) {
+    setSelectedId(id !== selectedId ? id : "");
+  }
+
+  return (
+    <div className="flashcards">
+      {questions.map((question) => (
+        <div
+          key={question.id}
+          className={question.id === selectedId ? "selected" : ""}
+          onClick={() => handleClick(question.id)}
+        >
+          <p>
+            {question.id === selectedId ? question.answer : question.question}
+          </p>
+        </div>
+      ))}
+    </div>
+  );
+}
